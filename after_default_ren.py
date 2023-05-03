ba0b18b721d745489ab42efc82c5e69a4ad80737
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-05-03 10:41:41 +0100

    fixed vindictive characters

--- after_default_ren.py
+++ after_default_ren.py
@@ -7,27 +7,27 @@ init python:
 
 def npc_setup() -> None:
     store.chloe.is_competitive = True
-    store.chloe.vindictive_characters = store.nora
+    store.chloe.vindictive_characters = (store.nora,)
 
     store.amber.is_competitive = True
     store.amber.is_talkative = True
-    store.amber.vindictive_characters = store.riley
+    store.amber.vindictive_characters = (store.riley,)
 
     store.riley.is_competitive = True
     store.riley.is_talkative = True
 
     store.lindsey.is_competitive = True
     store.lindsey.is_talkative = True
-    store.lindsey.vindictive_characters = store.chloe
+    store.lindsey.vindictive_characters = (store.chloe,)
 
     store.nora.is_talkative = True
     store.nora.vindictive_characters = (store.chris, store.chloe)
 
     store.aubrey.is_competitive = True
 
-    store.ryan.vindictive_characters = store.imre
+    store.ryan.vindictive_characters = (store.imre,)
 
-    store.imre.vindictive_characters = store.ryan
+    store.imre.vindictive_characters = (store.ryan,)
 
     store.charli.is_competitive = True
 
