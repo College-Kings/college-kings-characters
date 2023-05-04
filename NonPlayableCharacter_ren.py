a8acfd2c3904a912e8439df3c9973ac44b1bb6d5
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-05-04 23:06:38 +0100

    improved console output for NPCs

--- NonPlayableCharacter_ren.py
+++ NonPlayableCharacter_ren.py
@@ -46,3 +46,9 @@ class NonPlayableCharacter:
 
     def __hash__(self) -> int:
         return hash(self.name)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.name})"
+
+    def __str__(self) -> str:
+        return self.name
