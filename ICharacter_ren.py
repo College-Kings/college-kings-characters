dc57806ea360a85aa5548966608a00bb5565f144
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-05-06 19:30:41 +0100

    update ICharacter

--- ICharacter_ren.py
+++ ICharacter_ren.py
@@ -3,6 +3,7 @@ from abc import ABC
 from game.characters.Moods_ren import Moods
 
 from game.characters.Relationship_ren import Relationship
+from game.phone.Message_ren import Message
 
 """renpy
 init python:
@@ -10,5 +11,23 @@ init python:
 
 
 class ICharacter(ABC):
+    name: str
+    username: str
+
     relationships: dict[ICharacter, Relationship]
     mood: Moods
+
+    profile_pictures: list[str]
+    profile_picture: str
+    points: int
+    has_had_sex_with_mc: bool
+
+    is_competitive: bool
+    vindictive_characters: tuple[ICharacter, ...]
+    is_talkative: bool
+
+    pending_text_messages: list[Message]
+    text_messages: list[Message]
+
+    pending_simplr_messages: list[Message]
+    simplr_messages: list[Message]
