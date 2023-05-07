f1beb1770bca50c857dd3afb826f5ba97d0a5a94
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-05-07 04:21:21 +0100

    update missing relationship values

--- Relationship_ren.py
+++ Relationship_ren.py
@@ -1,5 +1,10 @@
+from __future__ import annotations
 import enum
 from enum import Enum
+
+from renpy import config
+
+_version: tuple[int, int, int]
 
 """renpy
 init python:
@@ -28,5 +33,19 @@ class Relationship(Enum):
     GIRLFRIEND = enum.auto()
 
     @classmethod
-    def _missing_(cls, value):
+    def _missing_(cls, value: object) -> Relationship:
+        if isinstance(_version, str) or _version < config.version:  # type: ignore
+            if value == -5:
+                return cls.EX
+            elif value == -4:
+                return cls.MAD
+            elif value == -3:
+                return cls.THREATEN
+            elif value == -2:
+                return cls.MAKEFUN
+            elif value == -1:
+                return cls.AWKWARD
+            elif value == 0:
+                return cls.FRIEND
+
         return cls.FRIEND
