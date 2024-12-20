f7c679cd07efecebfe021d143bf4b73c736d5a75
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-12-20 11:59:16 +0000

    Add name input method to MainCharacter for user-defined character names

--- main_character_ren.py
+++ main_character_ren.py
@@ -1,6 +1,10 @@
 from PlayableCharacters_ren import PlayableCharacter
 from Frat_ren import Frat
 from game.minigames.kings.kings_data_ren import KingsData
+from renpy.minstore import _
+from renpy import store
+import renpy.exports as renpy
+
 
 name = "Alex"
 
@@ -24,6 +28,13 @@ class MainCharacter(PlayableCharacter, object):
 
     def __repr__(self) -> str:
         return super().__repr__()
+
+    def name_input(
+        self, input_text: str = "What's your name?", default: str = "Alex"
+    ) -> None:
+        value = renpy.input(_(input_text), default=_(default)).strip() or _("Alex")
+        store.name = value
+        self._username = value
 
     @property
     def kings(self) -> "KingsData":
